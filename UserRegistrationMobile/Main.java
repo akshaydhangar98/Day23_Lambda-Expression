4900140dd4f8aeb9d5378d10eb02e3d7494b6f17
akshay dhangar <akshaygdhangar14@gmail.com>
2022-06-28 17:29:07 +0530

    UC_4

---
+++ UserRegistrationMobile/Main.java
@@ -0,0 +1,28 @@
+package UserRegistrationMobile;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		UserRegistration userregistration = new UserRegistration();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the FirstName");
+		String firstName = sc.nextLine();
+		userregistration.validateFirstName(firstName);
+		
+		System.out.println("Enter the LastName");
+		String lastname = sc.nextLine();
+		userregistration.validateLastName(lastname);
+		
+		System.out.println("Enter the Email");
+		String emailId = sc.nextLine();
+		UserRegistration.validateEmail(emailId);
+		
+		System.out.println("Enter the Mobile No");
+		String mobileNo = sc.nextLine();
+		UserRegistration.validateMobileNo(mobileNo);
+		
+		
+	}	
+}
